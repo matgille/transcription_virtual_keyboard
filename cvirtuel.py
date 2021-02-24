e5aa668fe7d391346625360b6e300a2c4162a5bd
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2021-02-24 15:30:03 +0100

    Ajout fonction recherche de caractères + création d'une classe VirtualKeyboard

--- cvirtuel.py
+++ cvirtuel.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3.7
 from tkinter import *
 import pyautogui
 import pyperclip
@@ -5,26 +6,60 @@ import json
 import time
 
 
-def main(characters):
-    print(f"There are {len(characters)} characters")
-    new_order = order_characters(characters)
-    print(f"Target characters:\n{characters}")
-    print(f"New order:\n{new_order}")
-    root = Tk()
-    root.title("cvirtuel")
-    labelfont = ('times', 16, 'bold')  # family, size, style
+class VirtualKeyboard:
+    def __init__(self, characters):
+        print(f"There are {len(characters)} characters")
+        new_order = order_characters(characters)
+        print(f"Target characters:\n{characters}")
+        print(f"New order:\n{new_order}")
+        root = Tk()
+        root.title("cvirtuel")
+        labelfont = ('times', 16, 'bold')  # family, size, style
+        default_bg_color = root.cget('bg')
 
-    exit_button = Button(root, text='Save/Exit', command=save_and_exit)
-    exit_button.grid(column=1, columnspan=2, row=1)
-    update_grid_button = Button(root, text='Update', command=lambda: update_grid(root, new_order)) #https://stackoverflow.com/a/6921225
-    update_grid_button.grid(column=3, columnspan=2, row=1)
+        exit_button = Button(root, text='Save/Exit', command=save_and_exit)
+        exit_button.grid(column=0, columnspan=2, row=1)
+        update_grid_button = Button(root, text='Update',
+                                    command=lambda: update_grid(root, new_order,
+                                                                default_bg_color))  # https://stackoverflow.com/a/6921225
+        update_grid_button.grid(column=2, columnspan=2, row=1)
+        search_form = Entry(root, textvariable='Search')
+        search_form.grid(row=1, column=6, columnspan=1)
+        search_form.config(fg='grey')
+        search_form.insert(END, "Search combined chars")  # voir https://stackoverflow.com/a/51781808 pour continuer
 
-    set_grid(labelfont, root, new_order)
+        global liste_object_character
+        liste_object_character = set_grid(labelfont, root, new_order, default_bg_color)
 
-    root.mainloop()
+        root.bind('<Return>', lambda _: get_character(search_form, liste_object_character))
+        search_form.bind("<Enter>", lambda _: handle_focus_in(search_form))
+        search_form.bind("<Leave>", lambda _: handle_focus_out(search_form, root))
+        root.mainloop()
 
 
-def set_grid(police, tkinter_racine, liste_caracteres):
+def get_character(form, liste):
+    print(f"Searched character: {form.get()}")
+    searched_character = form.get()
+    find_char_and_colour_it(searched_character, liste)
+
+
+def find_char_and_colour_it(character, liste):
+    '''
+    Cette fonction permet de changer le fond des boutons ayant un caractère précis.
+    :param character: le caractère à trouver
+    :param liste: la liste d'objets character() instanciée.
+    :return:
+    '''
+    # Ça ne marche aps quand on update car les objets sont détruits et remplacés par de nouveaux objets. Il
+    # faut mettre à jour la liste à chaque update.
+    matching_button_objects_index = [liste.index(button) for button in liste if character in button.char]
+    print(matching_button_objects_index)
+    for button in matching_button_objects_index:
+        print(liste[button].char)
+        liste[button].button.config(bg='red')
+
+
+def set_grid(police, tkinter_racine, liste_caracteres, default_bg_color):
     """
     Cette fonction permet d'organiser la grille par création d'instances de la classe `character`
     qui sont des boutons dont la position est déterminée par cette fonction.
@@ -35,12 +70,34 @@ def set_grid(police, tkinter_racine, liste_caracteres):
     """
     # Organisation de la grille
     n = 0
-    nombre_lignes = 11  # nombre de lignes de chaque colonne = hauteur de l'interface graphique.
+    nombre_lignes = 11  # nombre de lignes de chaque colonne: hauteur de l'interface graphique.
+    list_char_objects = []
     for char in liste_caracteres:
         ligne = (n % nombre_lignes) + 2
         colonne = (n // nombre_lignes)
-        character(char, tkinter_racine, police, colonne, ligne, liste_caracteres)
+        list_char_objects.append(
+            character(char, tkinter_racine, police, colonne, ligne, liste_caracteres, default_bg_color)
+        )
         n += 1
+    return list_char_objects
+
+# https://stackoverflow.com/a/51781808
+def handle_focus_in(search_form):
+    search_form.delete(0, END)
+    search_form.config(fg='black')
+    search_form.focus_set()
+
+def handle_focus_out(search_form, root):
+    search_form.delete(0, END)
+    search_form.config(fg='grey')
+    search_form.insert(0, "Search combined chars")
+    root.focus()
+
+def clean_color(default_bg_color):
+    global liste_object_character
+    for character_object in liste_object_character:
+        character_object.button.config(bg=default_bg_color)
+
 
 
 def save_and_exit():
@@ -59,14 +116,16 @@ def save_frequency():
         json.dump(stats_dict, json_file)
 
 
-def update_grid(root, liste_caracteres):
+def update_grid(root, liste_caracteres, default_bg_color):
     """
-    Cette fonction met à jour la grille en fonction de la fréquence.
-    :param root:
-    :param liste_caracteres:
-    :return:
+    Cette fonction met à jour la grille en fonction de la fréquence ainsi que la liste d'objets de classe character.
+    :param default_bg_color: La couleur du fond produite par défaut
+    :param root: l'instance tkinter produite
+    :param liste_caracteres: la liste de caractères originelle.
+    :return:None
     """
-    set_grid(('times', 16, 'bold'), root, order_characters(liste_caracteres))
+    global liste_object_character
+    liste_object_character = set_grid(('times', 16, 'bold'), root, order_characters(liste_caracteres), default_bg_color)
 
 
 def exit_app():
@@ -122,25 +181,26 @@ class character:
     dans le presse-papier quand on clique dessus.
     '''
 
-    def __init__(self, char, root, labelfont, column, row, liste_caracteres):
+    def __init__(self, char, root, labelfont, column, row, liste_caracteres, default_bg_color):
         self.position = [column, row]
         self.char = char
         self.root = root
         self.character_list = liste_caracteres
         self.labelfont = labelfont
-        self.button = Button(root, text=self.char, command=self.click)
+        self.button = Button(root, text=self.char, command=lambda: self.click(default_bg_color))
         self.button.grid(column=column, row=row)
         self.button.config(font=labelfont, height=1, width=3)
 
-    def __repr__(self):
-        print(self.char)
+    # def __repr__(self):
+    # print(self.char)
 
-    def click(self):
+    def click(self, default_bg_color):
         """
         On clique, on copie, on colle.
         :return: None
         """
         save_frequency()
+        clean_color(default_bg_color)
         print(f"You just clicked on {self.char}")
         self.add_to_frequency_table(self.char)
         pyperclip.copy(self.char)
@@ -179,6 +239,4 @@ if __name__ == '__main__':
         print("Veuillez créer le fichier characters.conf et y ajouter les caractères à afficher.")
         exit()
 
-
-
-    main(characters)
+    VirtualKeyboard(characters)
